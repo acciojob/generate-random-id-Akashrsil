33a10c8c7166fed9ba6366fc0fb8706ce06817ae
Akashrsil <159001656+Akashrsil@users.noreply.github.com>
2024-07-03 20:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
   // write your code here
+	let temp="1a2b3c4d5e";
+	let ans="";
+	let count=0;
+	while(count<=l)
+		{
+			let r=Math.floor(Math.random()*10);
+			ans=ans+temp[r];
+			count++;
+}
+	return ans;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+//const l = prompt("Enter a number.");
+//alert(makeid(l));
